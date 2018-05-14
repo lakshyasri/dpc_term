38ebb1ae90feaa7bfdca99081e768bcc090c48db
Lakshya Srivastava <lakshyasri@outlook.com>
2018-05-14 10:16:11 -0400

    Stock prediction program for slaves.

---
+++ stock.py
@@ -0,0 +1,37 @@
+import quandl
+import pandas as pd
+import numpy as np
+import datetime
+import scipy
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, cross_validation, svm
+
+df = quandl.get("WIKI/AMZN")
+print(df)
+#print(df.tail())
+
+df = df[['Adj. Close']]
+forecast_out = int(30)
+df['Prediction'] = df[['Adj. Close']].shift(-forecast_out)
+
+#print(df.tail())
+
+X = np.array(df.drop(['Prediction'], 1))
+X = preprocessing.scale(X)
+
+X_forecast = X[-forecast_out:]
+X = X[:-forecast_out]
+y = np.array(df['Prediction'])
+y = y[:-forecast_out]
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+# Training
+clf = LinearRegression()
+clf.fit(X_train,y_train)
+# Testing
+confidence = clf.score(X_test, y_test)
+#print("confidence: ", confidence)
+
+forecast_prediction = clf.predict(X_forecast)
+#print(forecast_prediction)
